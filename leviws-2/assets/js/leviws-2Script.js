f5f907e77664ba8f25719b31c1552b2aa6e2d552
Giuseppe010001 <158206013+Giuseppe010001@users.noreply.github.com>
2025-01-31 10:12:57 +0100

    Delete leviws-2 directory

--- leviws-2/assets/js/leviws-2Script.js
+++
@@ -1,7 +0,0 @@
-// Chiamata della funzione di gestione dell'effetto di transizione
-$(document).ready(transizioneFine);
-
-// Funzione finalizzata ad implementare l'entrata in dissolvenza dei loghi
-function transizioneFine() {
-    $(".transizioneInizio").delay(2000).fadeIn(3000);
-}
